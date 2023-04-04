eb78c92149ffbcd07646b75f336e39a9e2ae3f35
pranaydarshi <101990641+pranaydarshi@users.noreply.github.com>
2023-04-04 11:04:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,48 +3,61 @@ const app = express()
 const mongoose = require('mongoose')
 const Registeruser = require('./model')
 const jwt = require('jsonwebtoken')
-// const middleware = require('./middleware')
+const middleware = require('./middleware')
 app.use(express.json())
-const middleware = (req, res, next) => {
-    try {
+const data = [{
+    name:"pranay",
+    email:"pranay@gmail.com",
+    password:1234,
+    confirmpassword:1234,
+    id:1
+}]
 
-        let token = req.header('x-token')
-        if (!token) {
-            res.status(400).send("Token Not Found")
-        }
+// const middleware = (req, res, next) => {
+//     try {
 
-        let decode = jwt.verify(token, "jwtSecret");
-        req.user = decode.user
-        next()
+//         let token = req.header('x-token')
+//         if (!token) {
+//             res.status(400).send("Token Not Found")
+//         }
+
+//         let decode = jwt.verify(token, "jwtSecret");
+//         req.user = decode.user
+//         next()
 
 
-    } catch (err) {
-        console.error(err)
-        res.status(500).send("Server Error in Middleware")
-    }
-}
-mongoose.connect("mongodb+srv://pranaydarshi:pranaydarshi@cluster0.6bydbgh.mongodb.net/?retryWrites=true&w=majority").then(() => {
-    console.log("Database Connected Succesffuly");
+//     } catch (err) {
+//         console.error(err)
+//         res.status(500).send("Server Error in Middleware")
+//     }
+// }
+// mongoose.connect("mongodb+srv://pranaydarshi:pranaydarshi@cluster0.6bydbgh.mongodb.net/?retryWrites=true&w=majority").then(() => {
+//     console.log("Database Connected Succesffuly");
 
-}).catch(err => console.log(err))
+// }).catch(err => console.log(err))thu
 app.get('/', (req, res) => {
     res.send("Welcome Sai")
 })
 
 app.post("/register", async (req, res) => {
     try {
-        const { username, email, password, confirmpassword } = req.body;
-        let exist = await Registeruser.findOne({ email })
-        if (exist) {
-            res.status(400).send("User Already Exists")
-        }
-        if (password != confirmpassword) {
-            res.status(400).send("Passoword and confirmpassoword should be same")
-        }
-        let newUser = await Registeruser({
-            username, email, password, confirmpassword
-        })
-        await newUser.save()
+        const { username, email, password,confirmpassword,id } = req.body;
+        // let exist = await Registeruser.findOne({ email })
+        // let exist = data.find(em=>{
+        //     em.email === email;
+        // })
+        // if (exist) {
+        //     res.status(400).send("User Already Exists")
+        // }
+        // if (password != confirmpassword) {
+        //     res.status(400).send("Passoword and confirmpassoword should be same")
+        // }
+        // let newUser = await Registeruser({
+        //     username, email, password, confirmpassword
+        // })
+        data.push({username,email,password,id})
+
+        // await newUser.save()
         res.status(200).send('Registerd Successfully')
     } catch (err) {
         console.log(err.message);
@@ -54,22 +67,23 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
-        let exist = await Registeruser.findOne({ email })
-        if (!exist) {
-            res.status(400).send("User Not Exists")
-        }
-        if (exist.password != password) {
-            res.status(400).send("Invalid Credentials")
-        }
+        // let exist = await Registeruser.findOne({ email })
+        
+        // if (!exist) {
+        //     res.status(400).send("User Not Exists")
+        // }
+        // if (exist.password != password) {
+        //     res.status(400).send("Invalid Credentials")
+        // }
         let payload = {
-            user: {
-                id: exist.id
+            email,password
+                
             }
-        }
+        // }
         jwt.sign(payload, "jwtSecret", { expiresIn: 3600000 },
             (err, token) => {
                 if (err) throw err
-                res.json({ token })
+                res.json({email,token})
             }
         )
     } catch (err) {
@@ -78,13 +92,20 @@ app.post("/login", async (req, res) => {
     }
 })
 
-app.get('/myprofile', middleware, async (req, res) => {
+app.post('/myprofile', middleware, async (req, res) => {
     try {
-        let exist = await Registeruser.findById(req.user.id)
-        if (!exist) {
-            res.status(400).send("User Not Found")
-        }
-        res.json(exist)
+        // let exist = await Registeruser.findById(req.user.id)
+        // let exist = req.user.id;
+        // if (!exist) {
+        //     res.status(400).send("User Not Found")
+        // }
+        jwt.verify(req.token,"jwtSecret",(err,data)=>{
+            if(err){
+                res.status(500).send("Server Error")
+            }
+            res.send("Hello: "+data.email)
+        })
+        // res.json(data)
     } catch (err) {
         console.log(err.message)
         res.status(500).send("Sever Error")
